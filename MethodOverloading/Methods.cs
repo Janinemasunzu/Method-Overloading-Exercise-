f7a04b26869fe46dc88593c684d59e6bd4e88911
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-26 14:17:24 -0500

    done with exercise

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b) 
+        { 
+            return a + b;
+        }
+
+
+        public static decimal Add(decimal c, decimal d) 
+        {   
+            return c + d;
+        }
+
+        public static string Add(int e, int f, bool withDollars) 
+        {
+            if (withDollars && (e + f) != 1)
+            { 
+                return $"{e + f} dollars";
+            }
+            else if(withDollars && (e + f) == 1) 
+            { 
+                return $"{e + f} dollar";
+            
+            }
+            else 
+            { 
+                return $"{e + f}";
+            }
+        }
+        
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var sum = Methods.Add(4,5);
+            Console.WriteLine(sum);
+
+            var answer = Methods.Add(2.4m,4.2m);
+            Console.WriteLine(answer);
+
+            var result = Methods.Add(6, 4, true);
+            Console.WriteLine(result);
         }
     }
 }
